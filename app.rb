4b9ccbc86e65e0a238d87d648e454599c7289b65
Jan Kowalski <yakov85@o2.pl>
2021-08-19 17:05:49 +0200

    Navigation

--- app.rb
+++ app.rb
@@ -3,3 +3,19 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+get '/algorithms' do
+  erb :algorithms
+end
+
+get '/mathematics' do
+  erb :mathematics
+end
+
+get '/languages' do
+  erb :languages
+end
+
+get '/projects' do
+  erb :projects
+end
